b1c527a9a68183585df70eab5b374f6c86b13593
cecilioAlvarez <cmac@arquitecturajava.com>
2021-09-06 13:55:14 +0200

    nuevos ngif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { Hola1Component } from './hola1/hola1.component';
 import { Hola2Component } from './hola2/hola2.component';
 import { Hola3Component } from './hola3/hola3.component';
+import { Hola4Component } from './hola4/hola4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Hola1Component,
     Hola2Component,
-    Hola3Component
+    Hola3Component,
+    Hola4Component
   ],
   imports: [
     BrowserModule,
