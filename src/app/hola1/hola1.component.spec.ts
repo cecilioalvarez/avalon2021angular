740e2d115e803a961f5ae950c32653d74835e0a2
cecilioAlvarez <cmac@arquitecturajava.com>
2021-09-06 10:03:56 +0200

    componente inicial

---
+++ src/app/hola1/hola1.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Hola1Component } from './hola1.component';
+
+describe('Hola1Component', () => {
+  let component: Hola1Component;
+  let fixture: ComponentFixture<Hola1Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Hola1Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Hola1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
