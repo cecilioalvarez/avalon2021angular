6964e4e98aba5c9d74346ccc4f9e6f08145aca9e
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-09-06 12:38:57 +0200

    primeros ejemplos angular

--- src/app/hola1/hola1.component.ts
+++ src/app/hola1/hola1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Libro } from '../libro';
 
 @Component({
   selector: 'app-hola1',
@@ -8,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
 export class Hola1Component implements OnInit {
 
   nombre:string="pedro"
+  contador:number=0
+  libro: Libro 
+  paisaje:string
+  listaImagenes:string[]=["paisaje1.jfif","paisaje2.jfif","paisaje3.jpeg"]
+  constructor() { 
 
-  constructor() { }
+    this.libro=new Libro("1","java","pepe");
+    this.paisaje="assets/paisaje1.jfif"
+  }
 
   ngOnInit(): void {
+    this.paisaje='assets/'+this.listaImagenes[0];
+  }
+  incrementar(){
+    this.contador++;
+  }
+
+  decrementar(){
+    this.contador--;
+  }
+
+  pulsar(evento:Event){
+    alert("has pulsado")
+    console.log(evento);
+    
+  }
+  avanza(){
+    this.incrementar();
+    this.paisaje='assets/'+this.listaImagenes[this.contador];
+
+  }
+
+  retrocede(){
+    this.decrementar();
+    this.paisaje='assets/'+this.listaImagenes[this.contador];
+
   }
 
 }
