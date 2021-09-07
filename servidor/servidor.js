481638aedfa4c2cc9101e354738d3a2bc76ead79
cecilioAlvarez <cmac@arquitecturajava.com>
2021-09-07 14:20:49 +0200

    borrado con promesas angular

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -3,7 +3,7 @@ var cors = require("cors");
 const app = express()
 app.use(cors());
 const port = 8080
-let listalibros=[];
+var  listalibros=[];
 listalibros.push({isbn:"1",titulo:"java",autor:"juan"});
 listalibros.push({isbn:"2",titulo:"java2",autor:"juan2"});
 
@@ -11,14 +11,15 @@ app.get('/libros', (req, res) => {
   res.send(listalibros)
 })
 
+
 app.delete('/libros/:isbn', (req, res) => {
   
-  let libroborrar=this.listaLibros.filter(function(e) {
+  let libroborrar=listalibros.filter(function(e) {
     return e.isbn==req.params.isbn;
   })[0];
 
-  let posicion= this.listaLibros.indexOf(libroborrar);
-  this.listaLibros.splice(posicion,1);
+  let posicion= listalibros.indexOf(libroborrar);
+  listalibros.splice(posicion,1);
 
 
 })
