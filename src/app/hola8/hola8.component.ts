754c4cf41a1b23f0d991be695518f470aac09279
RubenPS <rpozo1991@gmail.com>
2021-09-08 10:34:51 +0200

    modificaciones para obsevable

--- src/app/hola8/hola8.component.ts
+++ src/app/hola8/hola8.component.ts
@@ -1,6 +1,8 @@
+import { mergeMap } from 'rxjs/operators';
 import { LibrosRestService } from './../rest/libros-rest.service';
 import { Component, OnInit } from '@angular/core';
 import { Libro } from '../libro';
+
 
 @Component({
   selector: 'app-hola8',
@@ -13,20 +15,31 @@ export class Hola8Component implements OnInit {
 
   constructor(public servicio: LibrosRestService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {/*  HECHO CON PROMISE
       this.servicio.buscarTodos().then((libros)=>{
         this.listaLibros=libros;
       }).catch(function(e){
         console.log(e)
-      })
-  }
+      })*/
 
-  borrar(libro: Libro){
+      this.servicio.buscarTodos().subscribe((libros)=>{
+        this.listaLibros=libros;
+      })
+
+  }
+/*      HECHO CON PROMISE
+  borrar(libro: Libro){       
     this.servicio.borrar(libro).then(()=>this.servicio.buscarTodos()).then((libros) => {
       console.log(libros)
       this.listaLibros=libros
     }).catch(function(error){
       console.log(error)
+    })
+  }*/
+
+  borrar(libro: Libro){
+    this.servicio.borrar(libro).pipe(mergeMap(e=>this.servicio.buscarTodos())).subscribe((libros)=>{
+      this.listaLibros=libros;
     })
   }
 }
--- src/app/rest/libros-rest.service.ts
+++ src/app/rest/libros-rest.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Libro } from '../libro';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class LibrosRestService {
 
   constructor(public http:HttpClient) { }
-
+/*
   buscarTodos():Promise<Libro[]> {
-    return this.http.get<Libro[]>("http://localhost:8080/libros").toPromise();
+    return this.http.get<Libro[]>("http://localhost:8080/webapi/libros").toPromise();
   }
 
   borrar(libro:Libro):Promise<Libro> {
-    return this.http.delete<Libro>(`http://localhost:8080/libros/${libro.isbn}`).toPromise();
+    return this.http.delete<Libro>(`http://localhost:8080/webapi/libros/${libro.isbn}`).toPromise();
+  }*/
+
+  buscarTodos():Observable<Libro[]> {
+    return this.http.get<Libro[]>("http://localhost:8080/webapi/libros");
+  }
+
+  borrar(libro:Libro):Observable<Libro> {
+    return this.http.delete<Libro>(`http://localhost:8080/webapi/libros/${libro.isbn}`);
   }
 }
