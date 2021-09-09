f216fc03c3fe6df2960b1eae1f9b73f495e052e4
unknown <molocale@gmail.com>
2021-09-09 14:02:37 +0200

    Location

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,5 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 import { Libro } from '../libro';
 import { LibrosRestService } from '../rest/libros-rest.service';
 
@@ -9,23 +12,28 @@ import { LibrosRestService } from '../rest/libros-rest.service';
   styleUrls: ['./detalle.component.css']
 })
 export class DetalleComponent implements OnInit {
-  libro:Libro={} as Libro;
-  constructor(private servicio: LibrosRestService, private route:ActivatedRoute) { }
+  libro: Libro = {} as Libro;
+ 
+  constructor(private servicio: LibrosRestService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
 
-    this.route.paramMap.subscribe((parametros) => {
-      let isbn = parametros.get("isbn");
+    this.route.paramMap.pipe(map((parametros) => {
+      return parametros.get("isbn");
+    })).pipe(mergeMap((parametro: string | null) => {
+      if (parametro != null)
+        return this.servicio.buscarUno(parametro);
+      else return new Observable<Libro>()
+    })).subscribe((parametro: Libro) => {
 
-      if (isbn != null)
-        this.servicio.buscarUno(isbn).subscribe((libro) => {
-
-          this.libro = libro;
-
-        })
+      this.libro = parametro;
 
     })
 
+  }
+
+  volver(){
+    this.location.back();
   }
 
 }
