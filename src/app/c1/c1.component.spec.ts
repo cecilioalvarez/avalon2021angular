52aff30535ae56578663f5712c5a08b453889f67
cecilioAlvarez <cmac@arquitecturajava.com>
2021-09-10 10:29:05 +0200

    nuevos pruebas unitarias

--- src/app/c1/c1.component.spec.ts
+++ src/app/c1/c1.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Persona } from '../persona';
 
 import { C1Component } from './c1.component';
 
@@ -16,6 +17,10 @@ describe('C1Component', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(C1Component);
     component = fixture.componentInstance;
+    component.listaPersonas.push(new Persona("pedro",20));
+    component.listaPersonas.push(new Persona("ana",50));
+    component.listaPersonas.push(new Persona("david",40));
+    component.listaPersonas.push(new Persona("maria",10));
     fixture.detectChanges();
   });
 
@@ -23,14 +28,50 @@ describe('C1Component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('test de verdadero', () => {
-    expect(true).toBe(true);
+  it('test de listaPersona inicializada', () => {
+
+    let persona= component.listaPersonas.find((persona)=>persona.nombre=="pedro");
+    expect(persona).not.toBeNull();
+    if (persona!=null) {
+      expect(persona.nombre).toBe("pedro");
+      expect(persona.edad).toBe(20);
+    }
+  
+  
+
+    
   });
 
-  it('test de suma de numeros', () => {
-    let numero=2;
-    let numero2=3;
-  
-    expect(numero+numero2).toBe(5);
+  it('test de listaPersona añadir una persona', () => {
+   
+    component.insertarPersona(new Persona("juan",50));
+   
+    let persona= component.listaPersonas.find((persona)=>persona.nombre=="juan");
+    expect(persona).not.toBeNull();
+    if (persona!=null) {
+      expect(persona.nombre).toBe("juan");
+      expect(persona.edad).toBe(50);
+    }
+
   });
+
+  it('test de listaPersona borrar una persona', () => {
+    let persona= new Persona("pedro",20);
+    component.borrarPersona(persona)
+
+    let personaBorrada= component.listaPersonas.find((persona)=>persona.nombre=="pedro");
+    expect(personaBorrada).not.toBeDefined()
+  });
+
+
+  it('test de suma edades en persona', () => {
+  
+    let total=component.sumarEdades(component.listaPersonas);
+
+   
+    expect(total).toBe(120);
+  });
+
+ 
+
 });
